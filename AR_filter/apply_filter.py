a4426c3c05bf57ed2d2a9237add9094d8d40ed6c
chenzihong-gavin <191250014@smail.nju.edu.cn>
2022-05-22 13:30:52 +0800

    feat: 完成特效按钮绑定

--- AR_filter/apply_filter.py
+++ AR_filter/apply_filter.py
@@ -1,46 +1,46 @@
+import os
+
 import mediapipe as mp
-import cv2
-import math
-import numpy as np
-import faceBlendCommon as fbc
 import csv
-
 VISUALIZE_FACE_POINTS = False
-
 filters_config = {
     'anonymous':
-        [{'path': "filters/anonymous.png",
-          'anno_path': "filters/anonymous_annotations.csv",
+        [{'path': "AR_filter/filters/anonymous.png",
+          'anno_path': "AR_filter/filters/anonymous_annotations.csv",
           'morph': True, 'animated': False, 'has_alpha': True}],
     'anime':
-        [{'path': "filters/anime.png",
-          'anno_path': "filters/anime_annotations.csv",
+        [{'path': "AR_filter/filters/anime.png",
+          'anno_path': "AR_filter/filters/anime_annotations.csv",
           'morph': True, 'animated': False, 'has_alpha': True}],
     'dog':
-        [{'path': "filters/dog-ears.png",
-          'anno_path': "filters/dog-ears_annotations.csv",
+        [{'path': "AR_filter/filters/dog-ears.png",
+          'anno_path': "AR_filter/filters/dog-ears_annotations.csv",
           'morph': False, 'animated': False, 'has_alpha': True},
-         {'path': "filters/dog-nose.png",
-          'anno_path': "filters/dog-nose_annotations.csv",
+         {'path': "AR_filter/filters/dog-nose.png",
+          'anno_path': "AR_filter/filters/dog-nose_annotations.csv",
           'morph': False, 'animated': False, 'has_alpha': True}],
     'cat':
-        [{'path': "filters/cat-ears.png",
-          'anno_path': "filters/cat-ears_annotations.csv",
+        [{'path': "AR_filter/filters/cat-ears.png",
+          'anno_path': "AR_filter/filters/cat-ears_annotations.csv",
           'morph': False, 'animated': False, 'has_alpha': True},
-         {'path': "filters/cat-nose.png",
-          'anno_path': "filters/cat-nose_annotations.csv",
+         {'path': "AR_filter/filters/cat-nose.png",
+          'anno_path': "AR_filter/filters/cat-nose_annotations.csv",
           'morph': False, 'animated': False, 'has_alpha': True}],
     'jason-joker':
-        [{'path': "filters/jason-joker.png",
-          'anno_path': "filters/jason-joker_annotations.csv",
+        [{'path': "AR_filter/filters/jason-joker.png",
+          'anno_path': "AR_filter/filters/jason-joker_annotations.csv",
           'morph': True, 'animated': False, 'has_alpha': True}],
     'gold-crown':
-        [{'path': "filters/gold-crown.png",
-          'anno_path': "filters/gold-crown_annotations.csv",
+        [{'path': "AR_filter/filters/gold-crown.png",
+          'anno_path': "AR_filter/filters/gold-crown_annotations.csv",
           'morph': False, 'animated': False, 'has_alpha': True}],
     'flower-crown':
-        [{'path': "filters/flower-crown.png",
-          'anno_path': "filters/flower-crown_annotations.csv",
+        [{'path': "AR_filter/filters/flower-crown.png",
+          'anno_path': "AR_filter/filters/flower-crown_annotations.csv",
+          'morph': False, 'animated': False, 'has_alpha': True}],
+    'green-carnival':
+        [{'path': "AR_filter/filters/green-carnival.png",
+          'anno_path': "AR_filter/filters/green-carnival_annotations.csv",
           'morph': False, 'animated': False, 'has_alpha': True}],
 }
 
@@ -132,7 +132,7 @@ def load_filter(filter_name="dog"):
     multi_filter_runtime = []
     for filter in filters:
         temp_dict = {}
-        img1, img1_alpha = load_filter_img(filter['path'], filter['has_alpha'])
+        img1, img1_alpha = load_filter_img( filter['path'], filter['has_alpha'])
         temp_dict['img'] = img1
         temp_dict['img_a'] = img1_alpha
         points = load_landmarks(filter['anno_path'])
@@ -144,7 +144,7 @@ def load_filter(filter_name="dog"):
             # Find Delaunay triangulation for convex hull points
             sizeImg1 = img1.shape
             rect = (0, 0, sizeImg1[1], sizeImg1[0])
-            dt = fbc.calculateDelaunayTriangles(rect, hull)
+            dt = calculateDelaunayTriangles(rect, hull)
 
             temp_dict['hull'] = hull
             temp_dict['hullIndex'] = hullIndex
@@ -159,7 +159,7 @@ def load_filter(filter_name="dog"):
     return filters, multi_filter_runtime
 
 
-def get_AR_Picture(image, name):
+def get_AR_Picture(image, name="dog"):
     # 变量设置
     count = 0
     isFirstFrame = True
@@ -188,7 +188,7 @@ def get_AR_Picture(image, name):
         d = cv2.norm(np.array(points2[k]) - points2Next[k])
         alpha = math.exp(-d * d / sigma)
         points2[k] = (1 - alpha) * np.array(points2[k]) + alpha * points2Next[k]
-        points2[k] = fbc.constrainPoint(points2[k], frame.shape[1], frame.shape[0])
+        points2[k] = constrainPoint(points2[k], frame.shape[1], frame.shape[0])
         points2[k] = (int(points2[k][0]), int(points2[k][1]))
 
 
@@ -229,8 +229,8 @@ def get_AR_Picture(image, name):
                     t1.append(hull1[dt[i][j]])
                     t2.append(hull2[dt[i][j]])
 
-                fbc.warpTriangle(img1, warped_img, t1, t2)
-                fbc.warpTriangle(img1_alpha_mask, mask1, t1, t2)
+                warpTriangle(img1, warped_img, t1, t2)
+                warpTriangle(img1_alpha_mask, mask1, t1, t2)
 
             # Blur the mask before blending
             mask1 = cv2.GaussianBlur(mask1, (3, 3), 10)
@@ -243,7 +243,7 @@ def get_AR_Picture(image, name):
             output = temp1 + temp2
         else:
             dst_points = [points2[int(list(points1.keys())[0])], points2[int(list(points1.keys())[1])]]
-            tform = fbc.similarityTransform(list(points1.values()), dst_points)
+            tform = similarityTransform(list(points1.values()), dst_points)
             # Apply similarity transform to input image
             trans_img = cv2.warpAffine(img1, tform, (frame.shape[1], frame.shape[0]))
             trans_alpha = cv2.warpAffine(img1_alpha, tform, (frame.shape[1], frame.shape[0]))
@@ -262,6 +262,9 @@ def get_AR_Picture(image, name):
         frame = output = np.uint8(output)
 
     return output
+
+def test(name):
+    print(os.getcwd()+"\\AR_filter\\")
 
 def get_AR_video(video_in, video_out):
     # 视频
@@ -285,6 +288,147 @@ def get_AR_video(video_in, video_out):
         print('视频打开失败！')
     writer.release()
     return 0
+
+import cv2
+import numpy as np
+import math
+
+
+# Constrains points to be inside boundary
+def constrainPoint(p, w, h):
+  p = (min(max(p[0], 0), w - 1), min(max(p[1], 0), h - 1))
+  return p
+
+# Compute similarity transform given two sets of two points.
+# OpenCV requires 3 pairs of corresponding points.
+# We are faking the third one.
+def similarityTransform(inPoints, outPoints):
+  s60 = math.sin(60*math.pi/180)
+  c60 = math.cos(60*math.pi/180)
+
+  inPts = np.copy(inPoints).tolist()
+  outPts = np.copy(outPoints).tolist()
+
+  # The third point is calculated so that the three points make an equilateral triangle
+  xin = c60*(inPts[0][0] - inPts[1][0]) - s60*(inPts[0][1] - inPts[1][1]) + inPts[1][0]
+  yin = s60*(inPts[0][0] - inPts[1][0]) + c60*(inPts[0][1] - inPts[1][1]) + inPts[1][1]
+
+  inPts.append([np.int(xin), np.int(yin)])
+
+  xout = c60*(outPts[0][0] - outPts[1][0]) - s60*(outPts[0][1] - outPts[1][1]) + outPts[1][0]
+  yout = s60*(outPts[0][0] - outPts[1][0]) + c60*(outPts[0][1] - outPts[1][1]) + outPts[1][1]
+
+  outPts.append([np.int(xout), np.int(yout)])
+
+  # Now we can use estimateRigidTransform for calculating the similarity transform.
+  tform = cv2.estimateAffinePartial2D(np.array([inPts]), np.array([outPts]))
+  return tform[0]
+
+
+# Check if a point is inside a rectangle
+def rectContains(rect, point):
+  if point[0] < rect[0]:
+    return False
+  elif point[1] < rect[1]:
+    return False
+  elif point[0] > rect[2]:
+    return False
+  elif point[1] > rect[3]:
+    return False
+  return True
+
+
+# Calculate Delaunay triangles for set of points
+# Returns the vector of indices of 3 points for each triangle
+def calculateDelaunayTriangles(rect, points):
+
+  # Create an instance of Subdiv2D
+  subdiv = cv2.Subdiv2D(rect)
+
+  # Insert points into subdiv
+  for p in points:
+    subdiv.insert((int(p[0]), int(p[1])))
+
+  # Get Delaunay triangulation
+  triangleList = subdiv.getTriangleList()
+
+  # Find the indices of triangles in the points array
+  delaunayTri = []
+
+  for t in triangleList:
+    # The triangle returned by getTriangleList is
+    # a list of 6 coordinates of the 3 points in
+    # x1, y1, x2, y2, x3, y3 format.
+    # Store triangle as a list of three points
+    pt = []
+    pt.append((t[0], t[1]))
+    pt.append((t[2], t[3]))
+    pt.append((t[4], t[5]))
+
+    pt1 = (t[0], t[1])
+    pt2 = (t[2], t[3])
+    pt3 = (t[4], t[5])
+
+    if rectContains(rect, pt1) and rectContains(rect, pt2) and rectContains(rect, pt3):
+      # Variable to store a triangle as indices from list of points
+      ind = []
+      # Find the index of each vertex in the points list
+      for j in range(0, 3):
+        for k in range(0, len(points)):
+          if(abs(pt[j][0] - points[k][0]) < 1.0 and abs(pt[j][1] - points[k][1]) < 1.0):
+            ind.append(k)
+        # Store triangulation as a list of indices
+      if len(ind) == 3:
+        delaunayTri.append((ind[0], ind[1], ind[2]))
+
+  return delaunayTri
+
+# Apply affine transform calculated using srcTri and dstTri to src and
+# output an image of size.
+def applyAffineTransform(src, srcTri, dstTri, size):
+
+  # Given a pair of triangles, find the affine transform.
+  warpMat = cv2.getAffineTransform(np.float32(srcTri), np.float32(dstTri))
+
+  # Apply the Affine Transform just found to the src image
+  dst = cv2.warpAffine(src, warpMat, (size[0], size[1]), None,
+             flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_REFLECT_101)
+
+  return dst
+
+# Warps and alpha blends triangular regions from img1 and img2 to img
+def warpTriangle(img1, img2, t1, t2):
+  # Find bounding rectangle for each triangle
+  r1 = cv2.boundingRect(np.float32([t1]))
+  r2 = cv2.boundingRect(np.float32([t2]))
+
+  # Offset points by left top corner of the respective rectangles
+  t1Rect = []
+  t2Rect = []
+  t2RectInt = []
+
+  for i in range(0, 3):
+    t1Rect.append(((t1[i][0] - r1[0]), (t1[i][1] - r1[1])))
+    t2Rect.append(((t2[i][0] - r2[0]), (t2[i][1] - r2[1])))
+    t2RectInt.append(((t2[i][0] - r2[0]), (t2[i][1] - r2[1])))
+
+  # Get mask by filling triangle
+  mask = np.zeros((r2[3], r2[2], 3), dtype=np.float32)
+  cv2.fillConvexPoly(mask, np.int32(t2RectInt), (1.0, 1.0, 1.0), 16, 0)
+
+  # Apply warpImage to small rectangular patches
+  img1Rect = img1[r1[1]:r1[1] + r1[3], r1[0]:r1[0] + r1[2]]
+
+  size = (r2[2], r2[3])
+
+  img2Rect = applyAffineTransform(img1Rect, t1Rect, t2Rect, size)
+
+  img2Rect = img2Rect * mask
+
+  # Copy triangular region of the rectangular patch to the output image
+  img2[r2[1]:r2[1]+r2[3], r2[0]:r2[0]+r2[2]] = img2[r2[1]:r2[1]+r2[3], r2[0]:r2[0]+r2[2]] * ((1.0, 1.0, 1.0) - mask)
+  img2[r2[1]:r2[1]+r2[3], r2[0]:r2[0]+r2[2]] = img2[r2[1]:r2[1]+r2[3], r2[0]:r2[0]+r2[2]] + img2Rect
+
 # filename = "05.jpg"
 # src = cv2.imread('E:\\'+filename)
 # magic_face = get_AR_Picture(src, "flower-crown")
